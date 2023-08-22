90a08e284027057c8d0f95a6bef58b7637eaf78a
Alyssa Woods <alyssawoods3@gmail.com>
2023-08-21 18:16:18 -0700

    Add ability to set subscription type/duration and calculate cost

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-// Your JS code here
+// Select elements
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+
+//Set subscription type and duration
+subTypeElement.addEventListener("change", function(e) {
+    subType = e.target.value;
+    updateSubscriptionDiv()
+});
+
+subDurationElement.addEventListener("change", function(e) {
+    subDuration = Number(e.target.value);
+    updateSubscriptionDiv()
+})
+
+//Calculate the cost of the subscrition
+var updateSubscriptionDiv = function () {
+    var monthlyCost = 5;
+    if (subType === "standard") {
+        monthlyCost = 7;
+    } else if (subType === "premium") {
+        monthlyCost = 10;
+    };
+    var total = subDuration * monthlyCost;
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
